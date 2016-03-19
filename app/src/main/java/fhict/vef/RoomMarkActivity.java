774234decdcf0ec42e57fb495de514a425528fd1
Thijsiez <thijsiez@hotmail.com>
2016-03-20 00:48:30 +0100

    Sort-of working MarkerTimeline

--- app/src/main/java/fhict/vef/RoomMarkActivity.java
+++ app/src/main/java/fhict/vef/RoomMarkActivity.java
@@ -4,17 +4,21 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
 import android.view.View;
 
 import fhict.vef.ui.MarkerTimelineView;
 
 public class RoomMarkActivity extends BaseRoomActivity {
+    //IT ISN'T EXACTLY A SECOND AS WE HAVE TO COMPENSATE FOR RUNNING THE TIMER LOGIC
+    private final int TIMER_TICK_PERIOD = 999;  //IN MILLISECONDS
+
     private MarkerTimelineView timeline;
+    private Handler timelineTimerHandler = new Handler();
+    private int tickCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +28,7 @@ public class RoomMarkActivity extends BaseRoomActivity {
         final View decorView = getWindow().getDecorView();
         if (decorView != null) {
             final int background = ((ColorDrawable)decorView.getBackground()).getColor(),
-                    target = Color.parseColor("#FF1744");
-
+                    target = Color.parseColor("#3F51B5");//TODO RECEIVE AND SET PERSONALIZED COLOR
             final ObjectAnimator backgroundAnimator = ObjectAnimator.ofObject(decorView,
                     "backgroundColor", new ArgbEvaluator(), background, target);
             backgroundAnimator.setDuration(1337);
@@ -33,8 +36,36 @@ public class RoomMarkActivity extends BaseRoomActivity {
         }
 
         timeline = (MarkerTimelineView)findViewById(R.id.room_mark_timeline);
+        final FloatingActionButton addMarker =
+                (FloatingActionButton)findViewById(R.id.room_mark_add_marker);
         if (timeline != null) {
-            
+            timelineTimerHandler.postDelayed(timerTick, TIMER_TICK_PERIOD);
+            if (addMarker != null) {
+                addMarker.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        timeline.addMarker();
+                    }
+                });
+            }
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        timelineTimerHandler.removeCallbacks(timerTick);
+    }
+
+    private final Runnable timerTick = new Runnable() {
+        @Override
+        public void run() {
+            if (timeline != null) {
+                timelineTimerHandler.postDelayed(this, TIMER_TICK_PERIOD);
+                timeline.setCurrentTimeInSeconds(++tickCount);
+            } else {
+                timelineTimerHandler.removeCallbacks(this);
+            }
+        }
+    };
 }
--- app/src/main/java/fhict/vef/ui/MarkerTimelineView.java
+++ app/src/main/java/fhict/vef/ui/MarkerTimelineView.java
@@ -6,23 +6,20 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.Point;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
+
+import java.util.ArrayList;
 
 import fhict.vef.R;
 
 public class MarkerTimelineView extends View {
-    private final String TAG = "MarkerTimeline";
-
-    //DIMENSIONS
     private final int MINIMUM_HEIGHT;
     private final int MARKER_WIDTH, MARKER_HEIGHT;
     private final Path MARKER_PATH;
 
-    //PAINTS
-    private final Paint PERSONAL_MARKER_PAINT;
+    private final ArrayList<Marker> markers = new ArrayList<>();
 
     private int secondsPerHalfWidth = 0;
     private int currentTimeInSeconds = 0;
@@ -34,35 +31,30 @@ public class MarkerTimelineView extends View {
     public MarkerTimelineView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        ////////////////////////////////////////
+        ////////////////////////////////////
         // RETRIEVE ALL CUSTOM ATTRIBUTES
         final TypedArray a = context.getTheme()
                 .obtainStyledAttributes(attrs, R.styleable.MarkerTimelineView, 0, 0);
         try {
             secondsPerHalfWidth = a
                     .getInteger(R.styleable.MarkerTimelineView_secondsPerHalfWidth, 0);
-            Log.d(TAG, String.format("Seconds per Half Width: %d", secondsPerHalfWidth));
         } finally {
             a.recycle();
         }
 
-        ////////////////////////////////////////////
-        // INITIALIZE ALL THE DIMENSIONS AND PAINTS
+        ////////////////////////////////////
+        // INITIALIZE ALL THE DIMENSIONS
         MINIMUM_HEIGHT = UITools.convertDpToPx(context, 64);
         MARKER_WIDTH = UITools.convertDpToPx(context, 24);
         MARKER_HEIGHT = UITools.convertDpToPx(context, 32);
 
-        PERSONAL_MARKER_PAINT = new Paint();
-        PERSONAL_MARKER_PAINT.setColor(Color.WHITE);
-        PERSONAL_MARKER_PAINT.setStyle(Paint.Style.FILL);
-
-        ////////////////////////////////////////////
+        ////////////////////////////////////
         // INITIALIZE PATHS
         MARKER_PATH = new Path();
-        MARKER_PATH.moveTo(MARKER_WIDTH / 2, 0);             //TOP CORNER
-        MARKER_PATH.lineTo(MARKER_WIDTH, MARKER_WIDTH / 2);   //RIGHT CORNER
-        MARKER_PATH.lineTo(MARKER_WIDTH / 2, MARKER_HEIGHT);  //BOTTOM CORNER
-        MARKER_PATH.lineTo(0, MARKER_WIDTH / 2);             //LEFT CORNER
+        MARKER_PATH.moveTo(MARKER_WIDTH / 2, 0);                //TOP CORNER
+        MARKER_PATH.lineTo(MARKER_WIDTH, MARKER_WIDTH / 2);     //RIGHT CORNER
+        MARKER_PATH.lineTo(MARKER_WIDTH / 2, MARKER_HEIGHT);    //BOTTOM CORNER
+        MARKER_PATH.lineTo(0, MARKER_WIDTH / 2);                //LEFT CORNER
         MARKER_PATH.close();
     }
 
@@ -76,30 +68,82 @@ public class MarkerTimelineView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        Log.v(TAG, "onDraw");
         canvas.drawColor(Color.MAGENTA);
-
-        canvas.save();
-        canvas.translate(getWidth() / 2 - MARKER_WIDTH / 2, getHeight() / 2 - MARKER_HEIGHT / 2);
-        canvas.drawPath(MARKER_PATH, PERSONAL_MARKER_PAINT);
-        canvas.restore();
+        for (Marker marker : markers) {
+            marker.draw(canvas);
+        }
     }
 
-    public int getSecondsPerHalfWidth() {
-        return secondsPerHalfWidth;
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        if (h != oldh) {
+            for (Marker marker : markers) {
+                marker.onParentChangedSize(w, h);
+            }
+        }
     }
 
-    public void setSecondsPerHalfWidth(int seconds) {
-        secondsPerHalfWidth = seconds > 0 ? seconds : 0;
-        invalidate();
-    }
-
-    public int getCurrentTimeInSeconds() {
-        return currentTimeInSeconds;
+    public void addMarker() {
+        markers.add(new Marker(Color.WHITE));
     }
 
     public void setCurrentTimeInSeconds(int seconds) {
         currentTimeInSeconds = seconds > 0 ? seconds : 0;
+        updateMarkerPositions();
         invalidate();
+    }
+
+    private void updateMarkerPositions() {
+        for (Marker marker : markers) {
+            marker.updateHorizontalPosition();
+        }
+    }
+
+    public class Marker {
+        private final int markTimeInSeconds;
+        private final Path path = new Path(MARKER_PATH);
+        private final Paint paint = new Paint();
+        private final Point position = new Point();
+
+        private int horizontalMidPosition = 0;
+        private int stepPixelsPerSecond = 0;
+        private boolean shouldDraw = true;
+
+        public Marker(int color) {
+            this.markTimeInSeconds = currentTimeInSeconds;
+            init(color);
+        }
+
+        private void init(int color) {
+            onParentChangedSize(getWidth(), getHeight());
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(color);
+        }
+
+        public void updateHorizontalPosition() {
+            position.x = horizontalMidPosition -
+                    stepPixelsPerSecond * (currentTimeInSeconds - markTimeInSeconds);
+
+            if (position.x < -MARKER_WIDTH) {
+                shouldDraw = false;
+            }
+        }
+
+        public void onParentChangedSize(int newWidth, int newHeight) {
+            horizontalMidPosition = newWidth / 2 - MARKER_WIDTH / 2;
+            position.y = newHeight / 2 - MARKER_HEIGHT / 2;
+            stepPixelsPerSecond = newWidth / 2 / secondsPerHalfWidth;
+        }
+
+        public void draw(Canvas canvas) {
+            if (shouldDraw) {
+                canvas.save();
+                canvas.translate(position.x, position.y);
+                canvas.drawPath(path, paint);
+                canvas.restore();
+            }
+        }
     }
 }
